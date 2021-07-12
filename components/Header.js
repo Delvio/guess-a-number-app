832af27cc5f74d49efde680d6fe5cf34489d385e
Delvio <elvioseverino@gmail.com>
2021-07-12 16:17:45 -0400

    fixed a typo

--- components/Header.js
+++ components/Header.js
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 90,
     paddingTop: 36,
-    backgroundColor: colors.primary,
+    backgroundColor: Colors.primary,
     alignItems: "center",
     justifyContent: "center",
   },
