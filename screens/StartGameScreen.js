c3cfab3b5864c93374532f5e00de0aab9ccd39fd
Delvio <elvioseverino@gmail.com>
2021-07-12 14:01:15 -0400

    added styles to card

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,29 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+
+import Card from "../components/Card";
+import Colors from "../constants/colors";
 
 const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
-      <Text>The Game Screen</Text>
+      <Text style={styles.title}>The Game Screen</Text>
+      <Card style={styles.inputContainer}>
+        <Text>Select A Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Reset" onPress={() => {}} color={Colors.primary} />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Confirm"
+              onPress={() => {}}
+              color={Colors.secondary}
+            />
+          </View>
+        </View>
+      </Card>
     </View>
   );
 };
@@ -14,6 +33,24 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  button: {
+    width: "40%",
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
   },
 });
 
