b92f36a29804bb1592d693207a244b02edb9b12f
Delvio <elvioseverino@gmail.com>
2021-07-15 13:28:11 -0400

    fixed typo error

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import * as Font from "expo-font";
-import { AppLoading } from "expo";
+import AppLoading from "expo-app-loading";
 // Screens
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
@@ -26,6 +26,7 @@ export default function App() {
       <AppLoading
         startAsync={fetchFonts}
         onFinish={() => setAssetLoaded(true)}
+        onError={() => console.log(err)}
       />
     );
   }
